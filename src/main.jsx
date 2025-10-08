5f36974fdc0c7c355fdbb3be488793aaec3199b8
a.danielswaroop <daniel.swrp@gmail.com>
2025-10-08 10:42:10 +0530

    setup github pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.jsx";
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
